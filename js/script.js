ec60f7642a817c67088e308875d1a140bd084f80
Danilo <danilo.colloca91@gmail.com>
2021-01-14 21:20:59 +0100

    .hover function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 /*seleziono il container del menu.al click avvio una funzione che quando cliccherò sull'elemento triggera la classe al suo opposto ovvero se la funzione hide è display none diventerà display block */
+
 $('.drop-down-container').click(function(){
   $(this).find('.dropDown-menu').toggleClass('hide');
 })
+
+
+/*aggiungendo la funzione hover in accoppiata alla funzione click otteniamo lo stesso comportamento dei menu originali*/
+$('.drop-down-container').hover(
+  function(){
+    $(this).find('.dropDown-menu').removeClass('hide');
+  }, function(){
+    $(this).find('.dropDown-menu').addClass('hide');
+
+  }
+);
